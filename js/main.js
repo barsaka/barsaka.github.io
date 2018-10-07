90b483439a5fb7918f52c0946ab1caa2a5c41be3
barsaka <38974058+barsaka@users.noreply.github.com>
2018-10-07 14:14:37 +0800

    fixed some css problem
    ...i forget to update the file and now the author's page is gone ; i want to cry

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+import { $ } from "jquery";
 $(document).ready(function(){
-    $("getElementsByClassName('rolling-head-right')").click(function(){
-      $("getElementById('rolling')").animate({left:'+=250px'});
+    $("document.getElementsByClassName('rolling-head-right')").click(function(){
+      $("document.getElementById('rolling')").animate({left:'+=250px'});
     });
   });
   window.onload=function(){
     $("p").hide();
-  });
+  };
   
   $("#show").click(function(){
     $("p").show();
